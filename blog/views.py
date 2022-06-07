211274c6601942c726d3eadc59c0690cdd37f198
ljbproductions <capamerica@email.com>
2022-06-07 12:14:10 -0700

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -17,16 +17,17 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST, request.FILES)
+        form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
+    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
